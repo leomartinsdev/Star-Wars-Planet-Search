4688140c4222d839a2a2007a429f393421eb4203
Leonardo Martins <leonardogustavo@outlook.com>
2024-04-25 15:13:48 -0300

    Deleted useless files and fixed some formatting

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import App from '../App';
-import PlanetsProvider from '../context/PlanetsProvider';
-import userEvent from '@testing-library/user-event';
-import mockData from './mockData';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "../App";
+import PlanetsProvider from "../context/PlanetsProvider";
+import userEvent from "@testing-library/user-event";
+import mockData from "./mockData";
 
-describe('App tests', () => {
+describe("App tests", () => {
   beforeEach(() => {
-    jest.spyOn(global, 'fetch');
+    jest.spyOn(global, "fetch");
     global.fetch = jest.fn().mockResolvedValue({
       json: jest.fn().mockResolvedValue(mockData),
     });
@@ -23,66 +23,59 @@ describe('App tests', () => {
     global.fetch.mockRestore();
   });
 
-  test('Testa se o usuário consegue digitar o nome de um planeta', () => {
-    const planetInput = screen.getByRole('textbox');
+  test("Testa se o usuário consegue digitar o nome de um planeta", () => {
+    const planetInput = screen.getByRole("textbox");
     expect(planetInput).toBeInTheDocument();
   });
 
-  test('Testa se existem os filtros', () => {
-    const columnFilter = screen.getByTestId('column-filter')
+  test("Testa se existem os filtros", () => {
+    const columnFilter = screen.getByTestId("column-filter");
     expect(columnFilter).toBeInTheDocument();
 
-    const comparisonFilter = screen.getByTestId('comparison-filter')
+    const comparisonFilter = screen.getByTestId("comparison-filter");
     expect(comparisonFilter).toBeInTheDocument();
 
-    const valueFilter = screen.getByTestId('value-filter')
+    const valueFilter = screen.getByTestId("value-filter");
     expect(valueFilter).toBeInTheDocument();
   });
 
-  test('Testa se é possível filtrar por nome', async () => {  // POR QUE NÃO FUNCIONA?
-    const planetInput = screen.getByRole('textbox');
+  test("Testa se é possível filtrar por nome", async () => {
     expect(planetInput).toBeInTheDocument();
     await screen.findByText(/Tatooine/i);
-    userEvent.type(planetInput, 'Ho');
+    userEvent.type(planetInput, "Ho");
 
-    // await waitFor(() => {
-    //   screen.getByText('Hoth');
-    // });
-
-    const hothPlanet = await screen.findByText(/Hoth/i)
+    const hothPlanet = await screen.findByText(/Hoth/i);
     expect(hothPlanet).toBeInTheDocument();
   });
 
-  test('Testa o funcionamento dos filtros numéricos', async () => {
+  test("Testa o funcionamento dos filtros numéricos", async () => {
     await screen.findByText(/Tatooine/i);
-    const columnFilter = screen.getByTestId('column-filter')
-    const comparisonFilter = screen.getByTestId('comparison-filter')
-    const valueFilter = screen.getByTestId('value-filter')
-    const filterBtn = screen.getByRole('button', {  name: /filtrar/i })
+    const columnFilter = screen.getByTestId("column-filter");
+    const comparisonFilter = screen.getByTestId("comparison-filter");
+    const valueFilter = screen.getByTestId("value-filter");
+    const filterBtn = screen.getByRole("button", { name: /filtrar/i });
 
-    userEvent.selectOptions(columnFilter, 'diameter');
-    userEvent.selectOptions(comparisonFilter, 'maior que');
-    userEvent.type(valueFilter, '10000');
+    userEvent.selectOptions(columnFilter, "diameter");
+    userEvent.selectOptions(comparisonFilter, "maior que");
+    userEvent.type(valueFilter, "10000");
     userEvent.click(filterBtn);
 
-
-    const row = screen.getAllByRole('row');
+    const row = screen.getAllByRole("row");
     expect(row.length).toBe(8);
 
-    userEvent.selectOptions(columnFilter, 'orbital_period');
-    userEvent.selectOptions(comparisonFilter, 'menor que');
-    userEvent.type(valueFilter, '400');
+    userEvent.selectOptions(columnFilter, "orbital_period");
+    userEvent.selectOptions(comparisonFilter, "menor que");
+    userEvent.type(valueFilter, "400");
     userEvent.click(filterBtn);
 
-    const rowsAfterFilter = screen.getAllByRole('row');
+    const rowsAfterFilter = screen.getAllByRole("row");
     expect(rowsAfterFilter.length).toBe(5);
   });
 
-  test('Testa se, no começo, todos planetas aparecem', async () => {
+  test("Testa se, no começo, todos planetas aparecem", async () => {
     await screen.findByText(/Tatooine/i);
 
-    const row = screen.getAllByRole('row');
+    const row = screen.getAllByRole("row");
     expect(row.length).toBe(11);
   });
-
 });
